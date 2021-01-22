76a8c675ffe05a6efd591dcbe94046f597a0bac7
Ratul-netizen <Ratul-netizen@github.com>
2021-01-22 15:22:27 +0600

    Final

--- assignment3.js
+++ assignment3.js
@@ -1,7 +1,7 @@
-// kilometer to meter conversion
+// Kilometer to meter conversion
 
 function kilometerToMeter(kilometer){
-   if(kilometer > 0){
+   if(kilometer > 0){   // Validity check
     var meter = kilometer * 1000;
     return meter;
    }
@@ -12,10 +12,10 @@ function kilometerToMeter(kilometer){
 
 
 
-// budget calculation
+// Budget calculation
 
 function budgetCalculator(watch, phone, laptop){
-    if(watch > 0 && phone > 0 && laptop > 0){
+    if(watch > 0 && phone > 0 && laptop > 0){  // validity check
     var price1 = watch * 50;
     var price2 = phone * 100;
     var price3 = laptop * 500;
@@ -30,28 +30,33 @@ function budgetCalculator(watch, phone, laptop){
 
 
 
-// hotel cost calculation
+// Hotel cost calculation
 
 function hotelCost(days){
     var totalCost = 0;
     
-   if(days > 0){
+   if(days > 0){  //validity check
 
+    //First condition at per day 100 taka
     if(days <= 10){
         totalCost = days * 100;
     }
+    
+    // Second condition at per day 80 taka
     else if(days <= 20){
-        var firstOffer = 10 * 100;
+        var firstCost = 10 * 100;
         var secondPhase = days - 10;
         var secondCost = secondPhase * 80;
-        totalCost = firstOffer + secondCost;
+        totalCost = firstCost + secondCost;
     }
+
+    // Third condition at per day 50 taka
     else{
-       var firstOffer = 10 * 100;
+       var firstCost = 10 * 100;
        var secondCost = 10 * 80;
        var thirdPhase = days - 20;
        var thirdCost = thirdPhase * 50;
-       var totalCost = firstOffer + secondCost + thirdCost;
+       var totalCost = firstCost + secondCost + thirdCost;
     }
     return totalCost;
    }
@@ -59,9 +64,10 @@ function hotelCost(days){
        return "Invalid Input";
    }
 }
+var cost = hotelCost(30);
+console.log(cost);
 
-
-// mega named friend
+// Mega named friend
 
 function megaFriend(names) {
 
